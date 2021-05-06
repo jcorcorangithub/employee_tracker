f016808e45fe99be1d6db7040a4af6e14735576e
James Corcoran <jpcsoccer1234@gmail.com>
2021-05-06 18:06:18 -0400

    began promt user functionality

--- index.js
+++ index.js
@@ -11,5 +11,103 @@ const connection = mysql.createConnection({
 
   connection.connect((err) => {
     if (err) throw err;
-    // this will probably be the function to start the prompt
+    promptUser();
   });
+
+  const promptUser = () => {
+      inquirer
+        .prompt({
+            name: 'action',
+            type: 'rawlist',
+            message: 'What would you like to do?',
+            choices: [
+                'add a department',
+                'add a role',
+                'add an employee',
+                'view a department',
+                'view a role',
+                'view an employee',
+                'update employee role',
+                // 'update employee managers',
+                // 'view employees by manager',
+                // 'delete a department',
+                // 'delete a role',
+                // 'delete an employee',
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'add a department':
+                    //function
+                    break;
+        
+                case 'add a role':
+                    //function
+                    break;
+        
+                case 'add an employee':
+                    //function
+                    break;
+        
+                case 'view a department':
+                    //function
+                    break;
+        
+                case 'view a role':
+                    //function
+                    break;
+        
+                case 'view an employee':
+                    //function
+                    break;
+
+                case 'update employee role':
+                    //function
+                    break;
+                
+                // case 'update employee managers':
+                //     //function
+                //     break;
+                    
+                // case 'view employees by manager':
+                //     //function
+                //     break;
+
+                // case 'delete a department':
+                //     //function
+                //     break;
+
+                // case 'delete a role':
+                //     //function
+                //     break;
+                
+                // case 'delete an employee':
+                //     //function
+                //     break;
+        
+                default:
+                  console.log(`Invalid action: ${answer.action}`);
+                  break;
+              }
+        });
+  };
+
+  const addDepartment = () => {
+      inquirer
+        .prompt([
+            {
+                name: 'id',
+                type: 'input',
+                message: "enter the id of the department",
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: "enter the name of the department",
+            },
+        ])
+        .then(function(answers){
+            //this is where the query will be written 
+            //the promptUser function will also be called again
+        });
+  };
--- schemas.sql
+++ schemas.sql
@@ -12,14 +12,13 @@ create table role (
     id int primary key,
     title varchar(30),
     salary decimal,
-    department_id int 
-    -- this will join department table
+    foreign key department_id references department(id),
 );
 
 create table employee (
     id int primary key,
     first_name varchar(30),
     last_name varchar(30),
-    role_id int, -- this will join role table
-    manager_id int 
+    foreign key role_id references role(id), 
+    foreign key mannager_id references role(id), 
 );
